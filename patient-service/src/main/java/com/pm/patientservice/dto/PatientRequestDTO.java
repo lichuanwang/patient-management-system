45da81699ad4637baafac3ad5083b80cbcdcf585
Li-Chuan Wang <wanglichuan127@gmail.com>
2025-12-31 11:51:53 +0800

    remove unecessary import in PatientRequestDTO

--- patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -3,7 +3,6 @@ package com.pm.patientservice.dto;
 import com.pm.patientservice.dto.validators.CreatePatientValidationGroup;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class PatientRequestDTO {
